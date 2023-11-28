f35ed037504ea7839ad368354c87c73b0f15dd03
dyto_aday <dyto_aday@hotmail.com>
2023-11-28 20:08:09 +0000

    2015-p1

---
+++ 2015/src/puzzle_1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    std::string nombreArchivo = "../input/puzzle_1.txt";
+    std::ifstream archivoEntrada(nombreArchivo);
+    if (!archivoEntrada.is_open())
+    {
+        std::cerr << "Error al abrir el archivo: " << nombreArchivo << std::endl;
+        return 1; 
+    }
+    char caracter;
+    int result;
+    while (archivoEntrada.get(caracter)) {
+        if (caracter == '(') result++;
+        else if (caracter == ')') result--;
+    }
+    archivoEntrada.close();
+    cout<<result;
+    return 0;
+}
